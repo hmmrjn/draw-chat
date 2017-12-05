a0dfecc44aea6cdd61207c38c78db2499f6fd899
hmmrjn <rodhamjun@hotmail.com>
2017-12-06 02:09:39 +0900

    added basic socket.io code to the server and the client now the clients can connect to the server

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,11 @@
+var socket;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(400, 400)
+    socket = io.connect('http://localhost:3000')
 }
 
 function draw() {
-    background(0);
-    ellipse(mouseX, mouseY, 40, 40);
+    background(0)
+    ellipse(mouseX, mouseY, 40, 40)
 }

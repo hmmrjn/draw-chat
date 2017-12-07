765c067c9cc447f1390fe006fb713e482b807417
hmmrjn <rodhamjun@hotmail.com>
2017-12-08 02:38:38 +0900

    self client traces are now shown with lines instead of balls
    other clients traces are still blue balls
    added class Point (consisting of x, y) and class Trace (a group of
    Points)

--- public/sketch.js
+++ public/sketch.js
@@ -18,33 +18,63 @@ function mouseMoved() {
     sendOwnMouseData()
 }
 
+//once when begun to press
+function mousePressed() {
+    ownTraces.push(new Trace())
+}
+
+//moved while pressed
 function mouseDragged() {
-    fill(255)
-    ownTraces.push({ x: mouseX, y: mouseY })
+    var newestOwnTrace = ownTraces[ownTraces.length - 1]
+    newestOwnTrace.points.push(new Point(mouseX, mouseY))
     sendOwnMouseData()
 }
 
 function draw() {
     background(255)
 
+    for (trace of ownTraces) {
+        trace.show()
+    }
+
+    noStroke()
+
     if (socket.id != null) {
         fill(0)
         text(socket.id, mouseX, mouseY - 10)
     }
 
-
-    for (trace of ownTraces) {
-        ellipse(trace.x, trace.y, 20, 20)
-    }
-
     fill(0, 0, 255)
     for (trace of otherCientsTraces) {
-        ellipse(trace.x, trace.y, 20, 20)
+        ellipse(trace.x, trace.y, 10, 10)
     }
 
     if (anotherClientMouseData != null) {
         fill(0, 0, 255)
         text(anotherClientMouseData.id, anotherClientMouseData.x, anotherClientMouseData.y)
+    }
+}
+
+
+class Point {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+}
+
+class Trace {
+    constructor(){
+        this.points = []
+    }
+    show() {
+        stroke(0)
+        strokeWeight(3)
+        for (var i = 0; i < this.points.length - 1; i++){
+            var p1 = this.points[i]
+            var p2 = this.points[i+1]
+            line(p1.x, p1.y, p2.x, p2.y)
+        }
     }
 }
 
